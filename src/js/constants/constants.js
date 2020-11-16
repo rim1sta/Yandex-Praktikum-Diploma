1b6df754ddd4084de75410cc39f37a98da115673
Konstantin <rim1sta@mail.ru>
2020-11-16 13:27:44 +0300

    edit baseUrl for news api

--- src/js/constants/constants.js
+++ src/js/constants/constants.js
@@ -5,10 +5,8 @@ export const ERROR_MESSAGES = {
 export const PAGE_SIZE = '100';
 export const FIRST_ARG = 0;
 export const SECOND_ARG = 3;
-export const NODE_ENV = process.env.NODE_ENV === "production";
 export const API_KEY = "5fc728e5d1e342cdb15adc005cc6e261";
-export const API_URL =
-  NODE_ENV === 'development' ? 'https://newsapi.org/v2/everything?': 'https://nomoreparties.co/news/v2/everything?';
+export const API_URL = process.env.NODE_ENV === 'development' ? 'https://newsapi.org/v2/everything?': 'https://nomoreparties.co/news/v2/everything?';
 
 export const DATE = new Date();
 export const CURRENT_DATE = `${DATE.getFullYear()}-${
